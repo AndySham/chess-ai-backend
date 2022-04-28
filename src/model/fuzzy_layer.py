0851667dfdc028ad0287aaec3b2901ea5a92e0e1
Andrew Shamis <andranadu@gmail.com>
2022-04-28 13:29:32 +0100

    Graphy graphs

--- src/model/fuzzy_layer.py
+++ src/model/fuzzy_layer.py
@@ -4,30 +4,38 @@ from torch import nn, Tensor
 from dnf import format_dnf
 
 
+
+class FuzzyParam(nn.Module):
+    def __init__(self, shape: torch.Size):
+        super().__init__()
+        self.param = nn.Parameter(torch.zeros(shape), requires_grad=True)
+        self.reinitialize()
+
+    def initialize(self, shape: torch.Size):
+        ws = torch.rand(shape)
+        with torch.no_grad():
+            self.param[:] = ws.log() - (1 - ws).log()
+
+    def reinitialize(self):
+        self.initialize(self.param.shape)
+
+    def value(self) -> Tensor:
+        return torch.sigmoid(self.param)
+
 class FuzzySignedAxisOp(nn.Module):
     def __init__(
         self, in_features: int, out_features: int, logic: FuzzyLogic,
     ):
         super().__init__()
         self._logic = logic
-        self.weights = torch.nn.Parameter(
-            torch.rand(in_features, out_features) - 0.5, requires_grad=True
-        )
-        self.signs = torch.nn.Parameter(
-            torch.rand(in_features, out_features) - 0.5, requires_grad=True
-        )
-
-    def fuzzy_params(self):
-        return (
-            torch.sigmoid(self.weights),
-            torch.sigmoid(self.signs),
-        )
-
+        self.weights = FuzzyParam((in_features, out_features))
+        self.signs = FuzzyParam((in_features, out_features))
 
 class FuzzySignedConjunction(FuzzySignedAxisOp):
     def forward(self, input):
         input = input.unsqueeze(-1)
-        weights, signs = self.fuzzy_params()
+        weights = self.weights.value()
+        signs = self.signs.value()
         return self._logic.conjoin(
             self._logic.implies(weights, self._logic.bin_xnor(input, signs)), dim=1,
         )
@@ -36,7 +44,8 @@ class FuzzySignedConjunction(FuzzySignedAxisOp):
 class FuzzySignedDisjunction(FuzzySignedAxisOp):
     def forward(self, input):
         input = input.unsqueeze(-1)
-        weights, signs = self.fuzzy_params()
+        weights = self.weights.value()
+        signs = self.signs.value()
         return self._logic.disjoin(
             self._logic.bin_conjoin(weights, self._logic.bin_xnor(input, signs)), dim=1,
         )
@@ -48,25 +57,19 @@ class FuzzyUnsignedAxisOp(nn.Module):
     ):
         super().__init__()
         self._logic = logic
-        self.weights = torch.nn.Parameter(
-            torch.rand(in_features, out_features) - 0.5, requires_grad=True
-        )
-
-    def fuzzy_params(self):
-        return torch.sigmoid(self.weights)
-
+        self.weights = FuzzyParam((in_features, out_features))
 
 class FuzzyUnsignedConjunction(FuzzyUnsignedAxisOp):
     def forward(self, input):
         input = input.unsqueeze(-1)
-        weights = self.fuzzy_params()
+        weights = self.weights.value()
         return self._logic.conjoin(self._logic.implies(weights, input), dim=1)
 
 
 class FuzzyUnsignedDisjunction(FuzzyUnsignedAxisOp):
     def forward(self, input):
         input = input.unsqueeze(-1)
-        weights = self.fuzzy_params()
+        weights = self.weights.value()
         return self._logic.disjoin(self._logic.bin_conjoin(weights, input), dim=1)
 
 
@@ -81,4 +84,3 @@ class FuzzyDNF(nn.Module):
 
     def forward(self, input: Tensor) -> Tensor:
         return self.layer(input)
-
